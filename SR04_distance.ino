343d1baffbdade4f7f74438e37e5831399b8cec7
iotstlabs <33818218+iotstlabs@users.noreply.github.com>
2019-04-09 14:28:32 +0900

    HC-SR04 Ultrasonic distance sensor

---
+++ SR04_distance.ino
@@ -0,0 +1,25 @@
+#define TrigPin  5   // D1
+#define EchoPin  4   // D2
+ 
+void setup() { 
+  Serial.begin (9600);
+  pinMode(TrigPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+}
+ 
+void loop() {
+  long duration;
+  float distance;
+  
+  digitalWrite(TrigPin, LOW);  
+  delayMicroseconds(2);   
+  digitalWrite(TrigPin, HIGH);
+  delayMicroseconds(10);   
+  digitalWrite(TrigPin, LOW);  
+  duration = pulseIn(EchoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(500);
+}
